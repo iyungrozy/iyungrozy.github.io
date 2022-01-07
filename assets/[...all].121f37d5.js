d41e86eb1d0ae85c42b0910479456685678c2caa
iyungrozy <36526260+iyungrozy@users.noreply.github.com>
2022-01-07 18:03:54 +0700

    hellowork

---
+++ assets/[...all].121f37d5.js
@@ -0,0 +1,1 @@
+import{d as o,u as t,o as s,a,t as e,e as n}from"./vendor.b2489c74.js";var r=o({expose:[],setup(o){const{t:r}=t();return(o,t)=>(s(),a("div",null,e(n(r)("not-found")),1))}}),u={};"function"==typeof u&&u(r);export default r;
